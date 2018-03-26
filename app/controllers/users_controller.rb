5f15048ad112748ffdddf970ff1734dcad4ce04e
mbalsamo <github email address>
2018-03-26 17:30:01 -0400

    add validations and create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController
 
   def new
-
+    @user = User.new
   end
 
   def create
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    # @user.save
 
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+    # raise errors
   end
+
 end

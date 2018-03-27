76c55d1543480b19b4e41c9829b2c1da27e81101
mbalsamo <github email address>
2018-03-27 12:20:52 -0400

    create authenticate_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
 
+  def authenticate_user
+    if !logged_in?
+      redirect_to login_path
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,6 @@ class UsersController < ApplicationController
     end
   end
 
-
   private
 
   def user_params

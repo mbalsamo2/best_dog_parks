995ba68c08fe94f88dbc8d5f9999905b4bda7302
mbalsamo <github email address>
2018-03-27 11:45:43 -0400

    create current_user functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  # def authenticate_user
+  #   if !logged_in?
+  #     redirect_to login_path
+  #   end
+  # end
+  #
+  # def logged_in?
+  #   session[:user_id]
+  # end
 end

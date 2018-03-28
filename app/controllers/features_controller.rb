2c894c74ea19e31bd37a0748f0897fbcb98e4beb
mbalsamo <github email address>
2018-03-28 13:20:55 -0400

    create new_park_feature form

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,6 +1,8 @@
 class FeaturesController < ApplicationController
 
   def new
-
+    # binding.pry
+    @park = Park.find(params[:park_id])
+    @feature = Feature.new
   end
 end
--- app/models/park.rb
+++ app/models/park.rb
@@ -2,6 +2,7 @@ class Park < ApplicationRecord
   belongs_to :user
   has_many :park_features
   has_many :features, through: :park_features
+  accepts_nested_attributes_for :features
 
   validates :name, presence: true
 

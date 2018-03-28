c98e6e4dccb1c244eca74eea4017101c4b45488c
mbalsamo <github email address>
2018-03-28 11:51:05 -0400

    create park_feature routes

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,2 +1,6 @@
 class FeaturesController < ApplicationController
+
+  def new
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, :only => [:new, :create]
-  resources :parks
+
+  resources :parks do
+    resources :features, only: %i[index create new edit]
+    post '/features/new'
+  end
+
   resources :features
+
   post '/users/new'
   post '/parks/new'
   get '/auth/:provider/callback' => 'sessions#create', as: :auth_login

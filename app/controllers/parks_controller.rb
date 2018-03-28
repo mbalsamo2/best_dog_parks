e04d46a1dea9697554f57e8b8b7f328c64f566eb
mbalsamo <github email address>
2018-03-28 09:48:34 -0400

    add user_authentication to parks controller

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,4 +1,5 @@
 class ParksController < ApplicationController
+  before_action :authenticate_user, only: [:index]
 
   def index
     @parks = current_user.parks
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,4 @@
 class SessionsController < ApplicationController
-  # before_action :authenticate_user, only: [:create]
 
   def new
   end
@@ -18,26 +17,6 @@ class SessionsController < ApplicationController
         redirect_to login_path
       end
     end
-    # if auth_hash = request.env["omniauth.auth"]
-    #   oauth_email = request.env["omniauth.auth"]["info"]["email"]
-    #   if @user = User.find_by(email: oauth_email)
-    #     session[:user_id] = @user.id
-    #     redirect_to parks_path
-    #   else
-    #     oauth_name = request.env["omniauth.auth"]["info"]["first_name"]
-    #     @user = User.create(name: oauth_name, email: oauth_email, password: SecureRandom.hex)
-    #     session[:user_id] = @user.id
-    #     redirect_to parks_path
-    #   end
-    # else
-    #   @user = User.find_by(email: params[:email])
-    #   if @user && @user.authenticate(params[:password])
-    #     session[:user_id] = @user.id
-    #     redirect_to parks_path
-    #   else
-    #     redirect_to login_path
-    #   end
-    # end
   end
 
   def destroy

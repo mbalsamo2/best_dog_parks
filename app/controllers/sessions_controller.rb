9457145dd606d35b5c948b711a706134aa0053e7
mbalsamo <github email address>
2018-03-28 09:39:18 -0400

    refactor omniauth create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-
     if auth_hash = request.env["omniauth.auth"]
-      oauth_email = request.env["omniauth.auth"]["info"]["email"]
-      if @user = User.find_by(email: oauth_email)
-        session[:user_id] = @user.id
-        redirect_to parks_path
-      else
-        oauth_name = request.env["omniauth.auth"]["info"]["first_name"]
-        @user = User.create(name: oauth_name, email: oauth_email, password: SecureRandom.hex)
-        session[:user_id] = @user.id
-        redirect_to parks_path
-      end
+      @user = User.find_or_create_by_omniauth(auth_hash)
+      session[:user_id] = @user.id
+      redirect_to parks_path
     else
       @user = User.find_by(email: params[:email])
       if @user && @user.authenticate(params[:password])
@@ -26,6 +18,26 @@ class SessionsController < ApplicationController
         redirect_to login_path
       end
     end
+    # if auth_hash = request.env["omniauth.auth"]
+    #   oauth_email = request.env["omniauth.auth"]["info"]["email"]
+    #   if @user = User.find_by(email: oauth_email)
+    #     session[:user_id] = @user.id
+    #     redirect_to parks_path
+    #   else
+    #     oauth_name = request.env["omniauth.auth"]["info"]["first_name"]
+    #     @user = User.create(name: oauth_name, email: oauth_email, password: SecureRandom.hex)
+    #     session[:user_id] = @user.id
+    #     redirect_to parks_path
+    #   end
+    # else
+    #   @user = User.find_by(email: params[:email])
+    #   if @user && @user.authenticate(params[:password])
+    #     session[:user_id] = @user.id
+    #     redirect_to parks_path
+    #   else
+    #     redirect_to login_path
+    #   end
+    # end
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ApplicationRecord
   has_many :parks
   has_many :features, through: :parks
 
+
+  def self.find_or_create_by_omniauth(auth_hash)
+    self.where(email: auth_hash["info"]["email"]).first_or_create do |user|
+      user.name = auth_hash["info"]["first_name"]
+      user.password = SecureRandom.hex
+    end
+  end
+
 end

31ed3c173a91d602c320ec28d5a75f9cd8bdfec6
mbalsamo <github email address>
2018-04-27 12:27:12 -0400

    attempt at refactor to get instead of ajax

--- app/assets/javascripts/parks.js
+++ app/assets/javascripts/parks.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
   $("a#load_features").on("click", function(e) {
+
+    // $.get(this.href).success(function(data) {
+    //   $("div.park_features").html(data)
+    // })
+
     $.ajax({
       method: "GET",
       url: this.href
     }).done(function(data) {
         $("div.park_features").html(data);
-      });
+      })
+
     e.preventDefault();
   });
 })

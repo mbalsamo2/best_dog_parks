e407300804152cb1cde071aff9dc8ed3256b0f07
mbalsamo <github email address>
2018-04-27 09:42:47 -0400

    hard code ajax get request to log park features data

--- app/assets/javascripts/parks.js
+++ app/assets/javascripts/parks.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
   $("a#load_features").on("click", function(e) {
-    alert("hi")
+    $.ajax({
+      method: "GET",
+      url: "/parks/4/features"
+    })
+      .done(function(data) {
+        console.log(data)
+      })
+
     e.preventDefault();
   });
 })

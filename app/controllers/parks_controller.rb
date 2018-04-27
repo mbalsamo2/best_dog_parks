03defbbf2d0e7426a923d9948262c59b018f6d67
mbalsamo <github email address>
2018-04-27 13:01:42 -0400

    refactor render layout

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -61,7 +61,7 @@ class ParksController < ApplicationController
 
   def feature_index
     @park = Park.find(params[:park_id])
-    render 'parks/feature_index', :layout => false
+    render :layout => false
   end
 
   private

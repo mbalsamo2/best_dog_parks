990a3d016e6281394c622cf293ea07049453c255
mbalsamo <github email address>
2018-04-27 17:26:42 -0400

    add ajax request for new features form

--- app/assets/javascripts/parks.js
+++ app/assets/javascripts/parks.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     //   $("div.park_features").html(data)
     // })
 
+  // ajax request to show index of park features on park show page
     $.ajax({
       method: "GET",
       url: this.href
@@ -14,4 +15,18 @@ $(document).ready(function() {
 
     e.preventDefault();
   });
+
+  // ajax request to show new features form on park show page
+  $("input#new_feature").on("click", function(e) {
+    $.ajax({
+      method: "GET",
+      url: "/parks/4/features/new"
+    }).done(function(response) {
+      console.log(response)
+    })
+
+    e.preventDefault();
+  });
+
+
 })

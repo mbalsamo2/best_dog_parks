7c802308540e0b3fbe386e3de0bbb9d8a15b82af
mbalsamo <github email address>
2018-04-29 14:24:41 -0400

    get park features link to render each time without page refresh

--- app/assets/javascripts/parks.js
+++ app/assets/javascripts/parks.js
@@ -1,19 +1,36 @@
-$(document).ready(function() {
-  $("a#load_features").on("click", function(e) {
-
+$(document).on('turbolinks:load', function() {
+  $('a#load_features').on('click', function(e) {
     // $.get(this.href).success(function(data) {
     //   $("div.park_features").html(data)
     // })
-    e.preventDefault();
-  // ajax request to show index of park features on park show page
+    e.preventDefault()
+    // ajax request to show index of park features on park show page
+    console.log(this.href)
     $.ajax({
-      method: "GET",
-      url: this.href
+      method: 'GET',
+      url: this.href,
     }).done(function(data) {
-        $("div.park_features").html(data);
-      })
-  });
+      $('div.park_features').html(data)
+    })
+  })
 })
+
+// $(document).ready(function() {
+//   $("a#load_features").on("click", function(e) {
+//
+//     // $.get(this.href).success(function(data) {
+//     //   $("div.park_features").html(data)
+//     // })
+//     e.preventDefault();
+//   // ajax request to show index of park features on park show page
+//     $.ajax({
+//       method: "GET",
+//       url: this.href
+//     }).done(function(data) {
+//         $("div.park_features").html(data);
+//       })
+//   });
+// })
 
 // does not work
 // document.addEventListener("turbolinks:load", function() {

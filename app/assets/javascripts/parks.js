5230cadfaa36588c96bbc406f5773f609d4f3153
mbalsamo <github email address>
2018-05-02 10:52:36 -0400

    successfully hijack submit button and create url action

--- app/assets/javascripts/parks.js
+++ app/assets/javascripts/parks.js
@@ -1,6 +1,5 @@
+// REQUIREMENT 1: ajax request to show index of park features on park show page
 $(document).on('turbolinks:load', function() {
-
-  // REQUIREMENT 1: ajax request to show index of park features on park show page
   $('a#load_features').on('click', function(e) {
     e.preventDefault();
     console.log(this.href);
@@ -15,57 +14,55 @@ $(document).on('turbolinks:load', function() {
         // var newFeatureForm = '<form class="new-todo" id="new_feature" action="/features" accept-charset="UTF-8" method="post"><label for="feature_name">Create a New Feature</label><br><input type="text" name="feature[name]" id="feature_name" class="new_todo" placeholder="Feature Name"><br><br><input type="text" name="feature[rating]" id="feature_rating" class="new_todo" placeholder="Feature Rating (1-5)"><br><br><input type="text" name="feature[comment]" id="feature_comment" class="new_todo" placeholder="Feature Comment"><br><br><input type="submit"></form>';
         // var newFeatureForm = '<h3><%= link_to "Add a Park Feature", new_park_feature_path(@park), :id => "new_feature" %></h3>'
         // $('div.new_park_feature').html(newFeatureForm);
-      })
-      // history.pushState(null, null, '/features');
-    })
+    });
+  });
+});
 
-// attemp to get features to load through new feature object
-  // $('a#load_features').on('click', function(e) {
-  //   e.preventDefault()
-  //   console.log(this.href)
-  //   $.get(this.href).done(function(data) {
-  //     $('div.park_features').html("");
-  //     debugger
-  //     data.forEach(function(feature) {
-  //       var newFeature = new Feature(feature.name, feature.rating, feature.comment, feature.park_features);
-  //       var formattedFeature = newFeature.formatFeatureIndex();
-  //       $('div.park_features').append(formattedFeature);
-  //     })
-  //   })
+// submit new feature via ajax
+$(document).on('turbolinks:load', function() {
+  $('#new_feature').on('submit', function(e) {
+    e.preventDefault();
+    url = this.action
+    console.log(url)
+    debugger
+
+    data = {
+      'authenticity_token': $("input[name='authenticity_token']").val(),
+      'feature': {
+        'name': $("#feature_name").val(),
+        'rating': $("#feature_rating").val(),
+        'comment': $("#feature_comment").val(),
+      }
+    };
+    // $.ajax({
+    //   type: "POST",
+    //   url: url
+    //   data: data
+    //   success: function(response) {
+    //   debugger
+    // }
   // })
 
+  })
+});
 
-     // submit new feature via ajax
-  // $('form#new_feature').on('submit', 'form#new_feature', function(e) {
-  //     e.preventDefault()
-  //     $.ajax({
-  //       method: "POST",
-  //       url:
-  //       data:
-  //
-  //     })
-  //       console.log("hey")
-  //       alert("twas clicked!")
-  //   })
-
-})
 
 // ajax request to show new features form on park show page
-$(document).on('click', 'a#new_feature', function(e) {
-  e.preventDefault()
-  // console.log(this.href)
-  // $.get(this.href.done(function(data) {
-  //   $('div.new_park_feature').html(response);
-  // })
-
-  $.ajax({
-    method: "GET",
-    url: this.href
-  }).done(function(response) {
-    $('div.new_park_feature').html("");
-    $('div.new_park_feature').html(response);
-  })
-})
+// $(document).on('click', 'a#new_feature', function(e) {
+//   e.preventDefault()
+//   // console.log(this.href)
+//   // $.get(this.href.done(function(data) {
+//   //   $('div.new_park_feature').html(response);
+//   // })
+//
+//   $.ajax({
+//     method: "GET",
+//     url: this.href
+//   }).done(function(response) {
+//     $('div.new_park_feature').html("");
+//     $('div.new_park_feature').html(response);
+//   })
+// })
 
 // make feautre object
 function Feature(id, name, rating, comment) {
